e71671c1b665b55fe6bbd62f923e376253949fd3
David Collins-Cubitt <david486038@googlemail.com>
2019-01-27 11:42:51 +0000

    testcode.php (added images)

---
+++ custom-modules/test-program/config.php
@@ -0,0 +1,3 @@
+<?php
+$appname ="Test Application";
+?>
--- testcode.php
+++ testcode.php
@@ -1,9 +1,12 @@
 <?php
+
 // List Module Names (only Folders)
 foreach (glob("custom-modules/*",GLOB_ONLYDIR) as $file) {
+
     // Print out to screen
-    include ('$file/config.php');
-    echo "<a href='$file/module.php'>$file</a><br />\n";
+    include ($file . '/config.php');
+    echo "<img src='" . $file . "/icon.png' height='50px'><br />";
+    echo "<a href='$file/module.php'>$appname</a><br /><br />\n";
 }
 
 
