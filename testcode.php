e016fe962f7db937c34de9643290f1309129d889
David Collins-Cubitt <david486038@googlemail.com>
2019-01-27 11:27:42 +0000

    testcode.php (build modules list)

--- testcode.php
+++ testcode.php
@@ -1,6 +1,13 @@
 <?php
-// List files as links
-foreach (glob("modules/*") as $file) {
-    echo "<a href='$file'>$file</a><br />";
+// List Module Names (only Folders)
+foreach (glob("custom-modules/*",GLOB_ONLYDIR) as $file) {
+    // Print out to screen
+    include ('$file/config.php');
+    echo "<a href='$file/module.php'>$file</a><br />\n";
 }
+
+
+// SAMPLE
+// menuitem('Restart Apache','apache.png','restart-apache.php');
+
 ?>
