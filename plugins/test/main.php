a238156b3a2f4c80bac41cd710f78cdf943e4239
Ben Bewick <viperfang@gmail.com>
2019-01-27 17:13:11 +0000

    Test module

---
+++ plugins/test/main.php
@@ -0,0 +1,51 @@
+<?php
+    
+    class test
+    {
+        
+        function getName()
+        {
+            return "Test";
+        }
+        
+        function getDescription()
+        {
+            return "Dummy plugin for Plugin Manager Testing";
+        }
+        
+        function DependencyCheck()
+        {
+            return; // No dep checks, no massiv config options, nothin!
+        }
+        
+        function init()
+        {
+            return;
+        }
+        
+        function getStats()
+        {
+            return array(); // Nope, none!
+        }
+        
+        function getPages()
+        {
+            return array(
+                array('id'=>'test','title'=>'Test Module'),
+            );
+        }
+        
+        function loadPage($id)
+        {
+            switch($id)
+            {
+            case 'test':
+                return "Test module loaded";
+                break;
+            default: // If this runs, we got problems
+                return "Test module is not responsible for page '$id'";
+                break;
+            }
+        }
+        
+    }
