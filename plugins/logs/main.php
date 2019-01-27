494d84b263b25420698e9e13adb83125c2c64a8b
David Collins-Cubitt <david486038@googlemail.com>
2019-01-27 19:33:54 +0000

    logs module

--- plugins/logs/main.php
+++ plugins/logs/main.php
@@ -31,21 +31,51 @@
         function getPages()
         {
             return array(
-                array('id'=>'logview','title'=>'Log Viewer'),
+                // ID > icon name
+                array('id'=>'logs','title'=>'Log Viewer'),
             );
         }
+
+
 
         function loadPage($id)
         {
             switch($id)
             {
-            case 'logview':
-                return "Log View module loaded";
+            case 'logs':
+                return content();
                 break;
             default: // If this runs, we got problems
                 return "LogView module is not responsible for page '$id'";
                 break;
             }
+        }
+
+        // Content
+        function content() {
+            echo "
+            <form action='#' method='request'>
+            <select name='log[]'>
+            <option value='/var/log/kern.log'>Kernal</option>
+            <option value='/var/log/auth.log'>Authentication</option>
+            <option value='/var/log/debug.log'>Debug</option>
+            <option value='/var/log/messages'>Messages</option>
+            <option value='/var/log/boot.log'>Boot</option>
+            <option value='/var/log/dmesg'>Dmessage</option>
+            <option value='/var/log/apt/history.log'>Apt History</option>
+            <option value='/var/log/syslog'>syslog</option>
+            </select>
+            <input type='submit' name='submit' value='Go' />
+            </form>
+            ";
+
+            if(isset($_REQUEST['submit'])){
+                foreach ($_REQUEST['log'] as $logfile) {
+                    $printkern = shell_exec("sudo cat $logfile");
+                    echo "<div class='catout'>$printkern</div>";
+                }
+            }
+
         }
 
     }
